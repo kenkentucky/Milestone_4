79963f2ac3cfcb47b50fdefe7dcc6dddc9551dc8
kenkentucky <aiman.nukman@gmail.com>
2023-02-04 01:18:38 +0800

    changes in db

--- Webpage/settings.py
+++ Webpage/settings.py
@@ -77,19 +77,12 @@ WSGI_APPLICATION = 'Webpage.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sensor_api',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'127.0.0.1',
-        'PORT': '3306',
-
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': BASE_DIR /'db.sqlite3',
     }
 }
 
-
-# Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
