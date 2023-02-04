a69dfae987d71286213ece5f9d521946982525fa
kenkentucky <aiman.nukman@gmail.com>
2023-02-04 15:49:37 +0800

    TAets

--- sensor/views.py
+++ sensor/views.py
@@ -12,7 +12,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import render
-from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 @api_view(['GET','POST'])
