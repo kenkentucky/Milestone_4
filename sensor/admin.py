2560c1d9081aa17361028407230577703947cb97
kenkentucky <98228879+kenkentucky@users.noreply.github.com>
2023-02-19 17:38:17 +0800

    Update admin.py

--- sensor/admin.py
+++ sensor/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from . models import Sensors
+from . models import SensorList
 from .models import SensorData
 
 
 # Register your models here.
 
-admin.site.register(Sensors)
+admin.site.register(SensorList)
 admin.site.register(SensorData)
 
 
