5890bd75bc05c58bd777b18cab343a189f0ac9d6
kenkentucky <98228879+kenkentucky@users.noreply.github.com>
2023-02-19 17:38:44 +0800

    Update models.py

--- sensor/models.py
+++ sensor/models.py
@@ -3,10 +3,9 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Sensors(models.Model):
+class SensorList(models.Model):
     
     name = models.CharField(max_length=30, null = True, blank = False)
-    PIC = models.CharField(max_length=30, null = True, blank = False)
     room = models.CharField(max_length=30, null =True, blank=False)
     
     def __str__(self):
@@ -17,7 +16,6 @@ class SensorData(models.Model):
     
  
     sensor = models.ForeignKey(Sensors,blank=True, null =True, on_delete=models.CASCADE)
-    location = models.CharField(max_length=30, null = False, blank = False)
     value1 = models.CharField(max_length=30, null = False, blank = False)
     value2 = models.CharField(max_length=30, null = False, blank = False)
     reading_time = models.DateTimeField(auto_now_add=True)
